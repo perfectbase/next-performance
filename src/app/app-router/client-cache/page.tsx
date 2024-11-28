204efae3dfa586dc775a67c4df04c295c2cb6f9e
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2024-11-28 20:43:06 +0900

    give up on rewrites

--- src/app/app-router/client-cache/page.tsx
+++ src/app/app-router/client-cache/page.tsx
@@ -1,11 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+
 export default function Page() {
   return (
-    <div>
-      {
-        // client cache needs to be a global configuration at `next.config.ts`
-        // in the production deployment I'll use Vercel's rewrite rules to show the other branch.
-        'Implemented at the branch "client-cache".'
-      }
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Client Cache</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="mt-4">
+          <div>TODO</div>
+          <Link href="https://next-performance-git-client-cache-perfectbases-projects.vercel.app/app-router/client-cache">
+            <Button>Go To Client Cache</Button>
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
