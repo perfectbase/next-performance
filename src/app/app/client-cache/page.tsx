ce7ab1d51da1123a109f66daf3c536c232b49403
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2024-11-28 19:32:56 +0900

    Implement client cache

--- src/app/app/client-cache/page.tsx
+++ src/app/app/client-cache/page.tsx
@@ -1,11 +1,3 @@
 export default function Page() {
-  return (
-    <div>
-      {
-        // client cache needs to be a global configuration at `next.config.ts`
-        // in the production deployment I'll use Vercel's rewrite rules to show the other branch.
-        'Implemented at the branch "client-cache".'
-      }
-    </div>
-  );
+  return <div>TODO</div>;
 }
