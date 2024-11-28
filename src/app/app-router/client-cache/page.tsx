f4fee9a561e4b33f845576f2bc87896165becae1
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2024-11-28 20:51:32 +0900

    add redirect to client cache

--- src/app/app-router/client-cache/page.tsx
+++ src/app/app-router/client-cache/page.tsx
@@ -1,21 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Client Cache</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <div className="mt-4">
-          <div>TODO</div>
-          <Link href="https://next-performance-git-client-cache-perfectbases-projects.vercel.app/app-router/client-cache">
-            <Button>Go To Client Cache</Button>
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
+  redirect(
+    "https://next-performance-git-client-cache-perfectbases-projects.vercel.app/app-router/client-cache"
   );
 }
