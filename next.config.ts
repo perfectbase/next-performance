1d54ea845f3e75ba4b3b98bf98598357850af044
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2024-11-28 19:53:50 +0900

    Add client-cache rewrites

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,20 @@ const nextConfig: NextConfig = {
   experimental: {
     ppr: "incremental",
   },
+  async rewrites() {
+    return process.env.NODE_ENV === "production"
+      ? [
+          {
+            source: "/app/client-cache/:path*",
+            destination: "http://localhost:3000/app/client-cache/:path*",
+          },
+          {
+            source: "/app/client-cache",
+            destination: "http://localhost:3000/app/client-cache",
+          },
+        ]
+      : [];
+  },
 };
 
 export default nextConfig;
