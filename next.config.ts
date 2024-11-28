84cfa67f795e0d9eaf9bcb90f70f56385e9bed5e
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2024-11-28 19:28:59 +0900

    Implement client cache

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     ppr: "incremental",
+    staleTimes: {
+      dynamic: 30,
+      static: 180,
+    },
   },
 };
 
