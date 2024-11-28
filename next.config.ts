7a5d2b82edd87b1d064bc175302b81220f6d08dc
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2024-11-28 19:59:23 +0900

    fix copilot mistake

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,20 @@ const nextConfig: NextConfig = {
     ppr: "incremental",
   },
   async rewrites() {
+    // experimental.staleTimes is a global setting
+    // so I deployed it from another branch
+    // and this rewrites point to it.
     return process.env.NODE_ENV === "production"
       ? [
           {
             source: "/app/client-cache/:path*",
-            destination: "http://localhost:3000/app/client-cache/:path*",
+            destination:
+              "https://next-performance-git-client-cache-perfectbases-projects.vercel.app/app/client-cache/:path*",
           },
           {
             source: "/app/client-cache",
-            destination: "http://localhost:3000/app/client-cache",
+            destination:
+              "https://next-performance-git-client-cache-perfectbases-projects.vercel.app/app/client-cache",
           },
         ]
       : [];
