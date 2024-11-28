f990b36cd69921a27c1cbabba1acf41270bef7b4
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2024-11-28 20:10:19 +0900

    testing rewrites

--- next.config.ts
+++ next.config.ts
@@ -4,25 +4,6 @@ const nextConfig: NextConfig = {
   experimental: {
     ppr: "incremental",
   },
-  async rewrites() {
-    // experimental.staleTimes is a global setting
-    // so I deployed it from another branch
-    // and this rewrites point to it.
-    return process.env.NODE_ENV === "production"
-      ? [
-          {
-            source: "/app/client-cache/:path*",
-            destination:
-              "https://next-performance-git-client-cache-perfectbases-projects.vercel.app/app/client-cache/:path*",
-          },
-          {
-            source: "/app/client-cache",
-            destination:
-              "https://next-performance-git-client-cache-perfectbases-projects.vercel.app/app/client-cache",
-          },
-        ]
-      : [];
-  },
 };
 
 export default nextConfig;
