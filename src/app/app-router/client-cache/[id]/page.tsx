835972dca9aef9fd1618b813e17135bb5f04ac37
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2024-11-30 13:43:51 +0900

    fix client-cache and add feature tags

--- src/app/app-router/client-cache/[id]/page.tsx
+++ src/app/app-router/client-cache/[id]/page.tsx
@@ -1,22 +1,10 @@
-import { getDynamicData } from "@/components/dynamic-component";
-import { unstable_cache } from "next/cache";
+import { DynamicComponent } from "@/components/dynamic-component";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   return (
     <div>
       Static Title
-      <CachedDynamicComponent {...props} />
+      <DynamicComponent {...props} />
     </div>
   );
 }
-
-async function CachedDynamicComponent({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  return <div>{await getCachedDynamicData(id)}</div>;
-}
-
-const getCachedDynamicData = unstable_cache(getDynamicData);
