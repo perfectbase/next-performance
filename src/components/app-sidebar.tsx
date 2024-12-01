a8d7a4610255ddf12756445df2bf4887b52c48cd
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2024-12-01 20:33:57 +0900

    Add demos to home page

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -12,69 +12,9 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { demos } from "@/lib/demos";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
-const items = [
-  {
-    title: "Pages Router",
-    items: [
-      {
-        title: "Static",
-        url: "/pages-router/static",
-      },
-      {
-        title: "Dynamic",
-        url: "/pages-router/dynamic",
-      },
-    ],
-  },
-  {
-    title: "App Router",
-    items: [
-      {
-        title: "Static",
-        url: "/app-router/static",
-      },
-      {
-        title: "Dynamic",
-        url: "/app-router/dynamic",
-      },
-      {
-        title: "Suspense",
-        url: "/app-router/suspense",
-      },
-      {
-        title: "Loading",
-        url: "/app-router/loading",
-      },
-      {
-        title: "Edge Runtime",
-        url: "/app-router/edge-runtime",
-      },
-      {
-        title: "Client Cache",
-        url: "/app-router/client-cache",
-      },
-      {
-        title: "Server Cache",
-        url: "/app-router/server-cache",
-      },
-      {
-        title: "Partial Prerendering",
-        url: "/app-router/partial-prerendering",
-      },
-      {
-        title: "Prefetch",
-        url: "/app-router/prefetch",
-      },
-      {
-        title: "Almighty",
-        url: "/app-router/almighty",
-      },
-    ],
-  },
-];
 
 export function AppSidebar() {
   const pathname = usePathname();
@@ -83,7 +23,7 @@ export function AppSidebar() {
     <Sidebar>
       <SidebarHeader />
       <SidebarContent>
-        {items.map((group) => (
+        {demos.map((group) => (
           <SidebarGroup key={group.title}>
             <SidebarGroupLabel className="uppercase">
               {group.title}
